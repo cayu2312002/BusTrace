1a6646d0fe7300fd7549c01036cdfa7df908ac2c
Miguel Angel Urueña Riobo <cayu2312002@gmail.com>
2022-10-02 11:11:21 -0500

    Corrección Fallas

--- app/src/main/java/co/edu/udea/compumovil/gr05_20212/bustrace/MainActivity.kt
+++ app/src/main/java/co/edu/udea/compumovil/gr05_20212/bustrace/MainActivity.kt
@@ -1,11 +1,17 @@
 package co.edu.udea.compumovil.gr05_20212.bustrace
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        MyToolbar().show(this, "Bus Trace", false)
     }
 }
